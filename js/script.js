fc401c791ee5b21745af4da33da2931fcca44da1
Arydiane <arydianejardim@gmail.com>
2023-08-29 20:44:17 -0300

    adiciona objeto com as telas de inicio e jogo e cria função mudar tela

--- js/script.js
+++ js/script.js
@@ -4,98 +4,155 @@ sprites.src = 'img/sprites.png';
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
+let telaAtiva = {};
+
 const planoDeFundo = {
     spriteX: 390,
     spriteY: 0,
-    largura: 275, 
-    altura: 204, 
-    x: 0, 
-    y: canvas.height - 204, 
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
 
     desenha: function () {
 
-        context.fillStyle = '#70c5ce'; 
+        context.fillStyle = '#70c5ce';
         context.fillRect(0, 0, canvas.width, canvas.height)
 
         context.drawImage(
-            sprites, 
-            planoDeFundo.spriteX, planoDeFundo.spriteY, 
-            planoDeFundo.largura, planoDeFundo.altura, 
-            planoDeFundo.x, planoDeFundo.y, 
-            planoDeFundo.largura, planoDeFundo.altura, 
-        ); 
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
 
         context.drawImage(
-            sprites, 
-            planoDeFundo.spriteX, planoDeFundo.spriteY, 
-            planoDeFundo.largura, planoDeFundo.altura, 
-            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y, 
-            planoDeFundo.largura, planoDeFundo.altura, 
-        ); 
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
     }
 }
 
 const chao = {
     spriteX: 0,
     spriteY: 610,
-    largura: 224, 
-    altura: 112, 
-    x: 0, 
-    y: canvas.height - 112, 
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
 
     desenha: function () {
         context.drawImage(
-            sprites, 
-            chao.spriteX, chao.spriteY, 
-            chao.largura, chao.altura, 
-            chao.x, chao.y, 
-            chao.largura, chao.altura, 
-        ); 
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
 
         context.drawImage(
-            sprites, 
-            chao.spriteX, chao.spriteY, 
-            chao.largura, chao.altura, 
-            (chao.x + chao.largura), chao.y, 
-            chao.largura, chao.altura, 
-        ); 
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura,
+        );
     }
 }
 
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
-    largura: 33, 
-    altura: 24, 
-    x: 10, 
-    y: 50, 
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
     velocidade: 0,
-    gravidade: 0.25, 
+    gravidade: 0.25,
 
     atualiza: function () {
-        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade; 
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
         flappyBird.y = flappyBird.y + flappyBird.velocidade;
     },
 
     desenha: function () {
         context.drawImage(
-            sprites, 
-            flappyBird.spriteX, flappyBird.spriteY, 
-            flappyBird.largura, flappyBird.altura, 
-            flappyBird.x, flappyBird.y, 
-            flappyBird.largura, flappyBird.altura, 
-        ); 
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY,
+            flappyBird.largura, flappyBird.altura,
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+        );
     }
+}
+
+const mensagemGetReady = {
+    spriteX: 134,
+    spriteY: 0,
+    largura: 174,
+    altura: 152,
+    x: (canvas.width / 2) - 172 / 2,
+    y: 50,
+
+    desenha: function () {
+        context.drawImage(
+            sprites,
+            mensagemGetReady.spriteX, mensagemGetReady.spriteY,
+            mensagemGetReady.largura, mensagemGetReady.altura,
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.largura, mensagemGetReady.altura,
+        );
+    }
+}
+
+const Telas = {
+    INICIO: {
+        atualiza: function () {
+
+        },
+        click: function () {
+            mudaParaTela(Telas.JOGO);
+        },
+        desenha: function () {
+            planoDeFundo.desenha();
+            chao.desenha();
+            flappyBird.desenha();
+            mensagemGetReady.desenha();
+        }
+    },
+    JOGO: {
+        atualiza: function () {
+            flappyBird.atualiza();
+        },
+        desenha: function () {
+            planoDeFundo.desenha();
+            chao.desenha();
+            flappyBird.desenha();
+        }
+    }
+}
+
+function mudaParaTela(novaTela) {
+    telaAtiva = novaTela;
 }
 
 function loop() {
 
-    planoDeFundo.desenha(); 
-    chao.desenha(); 
-    flappyBird.atualiza();
-    flappyBird.desenha(); 
+    telaAtiva.desenha();
+    telaAtiva.atualiza();
 
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', () => {
+    if (telaAtiva.click) {
+        telaAtiva.click();
+    }
+})
+
+mudaParaTela(Telas.INICIO);
 loop(); 
