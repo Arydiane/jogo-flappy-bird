9d32c99a6c51100011be7692732fe47f4d18fbc0
Arydiane <arydianejardim@gmail.com>
2023-08-29 20:15:06 -0300

    adiciona objeto com sprite do plano de fundo

--- js/script.js
+++ js/script.js
@@ -2,4 +2,45 @@ const sprites = new Image();
 sprites.src = 'img/sprites.png';
 
 const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
+const context = canvas.getContext('2d');
+
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 275, 
+    altura: 204, 
+    x: 0, 
+    y: canvas.height - 204, 
+
+    desenha: function () {
+
+        context.fillStyle = '#70c5ce'; 
+        context.fillRect(0, 0, canvas.width, canvas.height)
+
+        context.drawImage(
+            sprites, 
+            planoDeFundo.spriteX, planoDeFundo.spriteY, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+            planoDeFundo.x, planoDeFundo.y, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+        ); 
+
+        context.drawImage(
+            sprites, 
+            planoDeFundo.spriteX, planoDeFundo.spriteY, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+        ); 
+    }
+}
+
+
+function loop() {
+
+    planoDeFundo.desenha(); 
+
+    requestAnimationFrame(loop);
+}
+
+loop(); 
