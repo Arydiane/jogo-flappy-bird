719950efdbf48e67e01f90516fb18e9b1eaeec91
Arydiane <arydianejardim@gmail.com>
2023-08-29 20:16:03 -0300

    adiciona objeto com sprite do chão

--- js/script.js
+++ js/script.js
@@ -35,10 +35,39 @@ const planoDeFundo = {
     }
 }
 
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224, 
+    altura: 112, 
+    x: 0, 
+    y: canvas.height - 112, 
+
+    desenha: function () {
+        context.drawImage(
+            sprites, 
+            chao.spriteX, chao.spriteY, 
+            chao.largura, chao.altura, 
+            chao.x, chao.y, 
+            chao.largura, chao.altura, 
+        ); 
+
+        context.drawImage(
+            sprites, 
+            chao.spriteX, chao.spriteY, 
+            chao.largura, chao.altura, 
+            (chao.x + chao.largura), chao.y, 
+            chao.largura, chao.altura, 
+        ); 
+    }
+}
+
+
 
 function loop() {
 
     planoDeFundo.desenha(); 
+    chao.desenha(); 
 
     requestAnimationFrame(loop);
 }
